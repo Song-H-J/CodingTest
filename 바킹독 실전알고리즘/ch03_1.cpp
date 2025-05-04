c2fc511e9aa52d09e529f5156dca3e9e4a6878fd
DESKTOP-2QAMBO5\shj <hj093811@gmail.com>
2025-05-04 22:04:47 +0900

    0504

--- 바킹독 실전알고리즘/ch03_1.cpp
+++ 바킹독 실전알고리즘/ch03_1.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 void insert(int idx, int num, int arr[], int& len){
-
+  for (int i = len; i >= idx; i--) { // O(N)
+    arr[i+1] = arr[i];
+  }
+  arr[idx] = num;
+  len = len+1;
 }
 
 void erase(int idx, int arr[], int& len){
-  
+  for (int i = idx; i <= len; i++) { // O(N)
+    arr[i] = arr[i+1];
+  }
+  arr[len] = 0;
+  len = len-1;
 }
 
 void printArr(int arr[], int& len){
