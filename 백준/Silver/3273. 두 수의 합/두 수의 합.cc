43c10e43179739dc0d7e1216b347a13cfadd5e61
DESKTOP-2QAMBO5\shj <hj093811@gmail.com>
2025-05-07 19:58:37 +0900

    0507

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -10,6 +10,13 @@ void calcMatch(int X, vector<int>& num)
     int ans = 0;
     for (auto iter = num.begin(); iter != num.end(); ++iter) {
         if(X >= *iter) {
+            //zToX[*iter]++; 여기에 쓰면 중복 카운트 문제 발생
+            /*예를 들어 X가 100이고 *iter가 50이면
+              50이 등장했을때 순서쌍 ans가 ++
+              하지만 서로 다른 수만 존재하기에 이 순서쌍은 불가능
+              그렇지만 이미 zToX의 50번째 원소값이 ++ 되었기에
+              가능한 순서쌍이란 결론이 나버림
+            */
             if (zToX[X - *iter] != 0)
                 ans++;
             zToX[*iter]++;
